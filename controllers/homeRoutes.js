bdd5bbb52cd163dafa0a75222ccb253256e597d9
Liza Parr <lizajparr@gmail.com>
2023-03-23 13:34:08 -0400

    correct seed data showing up in console

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const { User, Todos } = require('../models');
 
 router.get('/', async (req, res) => {
+  console.log(req.session);
+  const rawToDoData = await Todos.findAll({
+    where: {
+      user_id: req.session.userID,
+    },
+  });
+  let toDoData = [];
+  for (i = 0; i < rawToDoData.length; i++) {
+    toDoData.push(rawToDoData[i].get({ plain: true }));
+  }
+  console.log(toDoData);
   res.render('home', {
+    toDoData,
     loggedIn: req.session.loggedIn
   });
 });
