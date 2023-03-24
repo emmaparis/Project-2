71d66c640fe0376741b86ff03f063472385ff8b1
emmaparis <elp.dance@gmail.com>
2023-03-24 11:12:04 -0400

    delete route phase 1

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Todos } = require('../models');
+const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
   if (req.session.loggedIn) {
@@ -23,4 +24,25 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const todoData = await Todos.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!todoData) {
+      res.status(404).json({ message: 'No todo item found with this id!' });
+      return;
+    }
+
+    res.status(200).json(todoData);
+} catch (err) {
+    res.status(500).json(err);
+    }
+    });
+    
+    module.exports = router;
 module.exports = router;
