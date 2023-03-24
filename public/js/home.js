2dc85e41927f086d14497b713339002f48faeb54
Shannya <niveyro101@gmail.com>
2023-03-24 19:15:55 -0400

    button added to handle bar- starting modal

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,14 @@
 const logInButtonEl = document.querySelector(".logInButton")
 const signUpButtonEl = document.querySelector(".signUpButton")
 
+(document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
 //logInButtonEl.addEventListener()
 //send to log in page
 //signUpButtonEl.addEventListener()
