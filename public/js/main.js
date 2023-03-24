71a4edeee6a727fb111a243e37f49e6e4d73dede
Shannya <niveyro101@gmail.com>
2023-03-24 02:39:11 -0400

    looks ok

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,2 @@
 const dateTimeEl = document.querySelector('#date-time')
-dateTimeEl.textContent = "Today, " + dayjs().format("dddd, MMMM D, YYYY");
+dateTimeEl.textContent = `Today,  ${dayjs().format("dddd, MMMM D, YYYY")}`;
