8339cc05095071b9265abba487cd15e24d4b3d3b
Shannya <niveyro101@gmail.com>
2023-03-26 12:31:02 -0400

    ensure all fields are filled in

--- public/js/signup.js
+++ public/js/signup.js
@@ -11,27 +11,29 @@ const createAccount = async (event) => {
     const emailAcc = document.querySelector('#email').value.trim();
     const passwordAcc = document.querySelector('#password').value.trim();
     const passwordConfirmation = document.querySelector('#passwordCon').value.trim();
-    if (passwordAcc===passwordConfirmation){
-    //     if (todoText) {
-    //         const response = await fetch(`/add`, {
-    //           method: 'POST',
-    //           body: JSON.stringify({ todoText }),
-    //           headers: { 'Content-Type': 'application/json' },
-    //         });
+    document.querySelector("#pwErr").textContent = ""//rest err messages
+    document.querySelector("#inputErr").textContent = ""
+    if (nameAcc && emailAcc && passwordAcc && passwordConfirmation) {
+        console.log('all input')
+        if (passwordAcc===passwordConfirmation ){
+            console.log('all input')
+            // const response = await fetch(`/add`, {
+            //   method: 'POST',
+            //   body: JSON.stringify({ todoText }),
+            //   headers: { 'Content-Type': 'application/json' },
+            // });
         
-    //         if (response.ok) {
-    //           console.log("ok");
-    //           document.location.reload();
-    //         } else {
-    //           alert('Failed to add todo item');
-    //         }
-    //       }
-    }else{
-        console.log("pwds dont match")
-        document.querySelector("#pwErr").append("Passwords do not match. Try again.")
+            // if (response.ok) {
+            //   console.log("ok");
+            //   document.location.reload();
+            // } else {
+            //   alert('Failed to add todo item');
+            // }
+          }else {
+            document.querySelector("#pwErr").textContent = "Passwords do not match. Try again."
+        }
+    } else {
+        document.querySelector("#inputErr").textContent = "All fields must be filled in to make an account."
     }
 }
-    
-//   };
-  
 document.querySelector('#makeAccount').addEventListener('click', createAccount);
