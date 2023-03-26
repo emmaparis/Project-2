3939c99c47ce7ce466d4e4d7e6b9be39b120b1cf
Shannya <niveyro101@gmail.com>
2023-03-26 13:30:39 -0400

    what ive got for now

--- public/js/signup.js
+++ public/js/signup.js
@@ -7,33 +7,34 @@ const createAccount = async (event) => {
     // event.preventDefault();
     // console.log("pressed sign up");
     // // console.log(todoText);
-    const nameAcc = document.querySelector('#name').value.trim();
-    const emailAcc = document.querySelector('#email').value.trim();
-    const passwordAcc = document.querySelector('#password').value.trim();
+    const name = document.querySelector('#name').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
     const passwordConfirmation = document.querySelector('#passwordCon').value.trim();
     document.querySelector("#pwErr").textContent = ""//rest err messages
     document.querySelector("#inputErr").textContent = ""
-    if (nameAcc && emailAcc && passwordAcc && passwordConfirmation) {
+    if (name && email && password && passwordConfirmation) {
         console.log('all input')
-        if (passwordAcc===passwordConfirmation ){
-            console.log('all input')
-            // const response = await fetch(`/add`, {
-            //   method: 'POST',
-            //   body: JSON.stringify({ todoText }),
-            //   headers: { 'Content-Type': 'application/json' },
-            // });
+        if (password===passwordConfirmation ){
+        //     console.log('all input')
+        //     const response = await fetch(`/add`, {
+        //       method: 'POST',
+        //       body: JSON.stringify({ name, email, password }),
+        //       headers: { 'Content-Type': 'application/json' },
+        //     });
         
-            // if (response.ok) {
-            //   console.log("ok");
-            //   document.location.reload();
-            // } else {
-            //   alert('Failed to add todo item');
-            // }
-          }else {
+        //     if (response.ok) {
+        //       console.log("ok");
+        //       document.location.reload();
+        //     } else {
+        //       alert('Failed to add user.');
+        //     // }
+        //   }else {
             document.querySelector("#pwErr").textContent = "Passwords do not match. Try again."
         }
     } else {
         document.querySelector("#inputErr").textContent = "All fields must be filled in to make an account."
     }
 }
+
 document.querySelector('#makeAccount').addEventListener('click', createAccount);
