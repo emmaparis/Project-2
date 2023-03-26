cab6201d8c27f6d02ef1bc3caaf3de965ce3c17f
Shannya <niveyro101@gmail.com>
2023-03-26 11:41:14 -0400

    added query selectors to inputs

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,6 +1,30 @@
 const confirm = document.querySelector('#makeAccount');
 
-
+const createAccount = async (event) => {
+    event.preventDefault();
+    console.log("pressed sign up");
+    const nameAcc = document.querySelector('#name').value.trim();
+    const emailAcc = document.querySelector('#email').value.trim();
+    const passwordAcc = document.querySelector('#password').value.trim();
+    const passwordConfirmation = document.querySelector('#password2').value.trim();
+    console.log(todoText);
+    if (todoText) {
+      const response = await fetch(`/add`, {
+        method: 'POST',
+        body: JSON.stringify({ todoText }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        console.log("ok");
+        document.location.reload();
+      } else {
+        alert('Failed to add todo item');
+      }
+    }
+  };
+  
+  document.querySelector('#submit-todo-button').addEventListener('click', createTodoHandler);
 
 
 
