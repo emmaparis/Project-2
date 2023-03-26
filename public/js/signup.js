6fb1aaa25cc3c4a917239ab97aa91ea330667de2
Shannya <niveyro101@gmail.com>
2023-03-26 12:12:29 -0400

    password confirmation

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,36 +1,37 @@
-const confirm = document.querySelector('#makeAccount');
-
+// const subSignUp = document.querySelector('#makeAccount');
+// const nameAcc = document.querySelector('#name').value.trim();
+// const emailAcc = document.querySelector('#email').value.trim();
+// const passwordAcc = document.querySelector('#password').value.trim();
+// const passwordConfirmation = document.querySelecto('#passwordCon').value.trim();
 const createAccount = async (event) => {
-    event.preventDefault();
-    console.log("pressed sign up");
+    // event.preventDefault();
+    // console.log("pressed sign up");
+    // // console.log(todoText);
     const nameAcc = document.querySelector('#name').value.trim();
     const emailAcc = document.querySelector('#email').value.trim();
     const passwordAcc = document.querySelector('#password').value.trim();
-    const passwordConfirmation = document.querySelector('#password2').value.trim();
-    console.log(todoText);
-    if (todoText) {
-      const response = await fetch(`/add`, {
-        method: 'POST',
-        body: JSON.stringify({ todoText }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        console.log("ok");
-        document.location.reload();
-      } else {
-        alert('Failed to add todo item');
-      }
+    const passwordConfirmation = document.querySelector('#passwordCon').value.trim();
+    if (passwordAcc===passwordConfirmation){
+    //     if (todoText) {
+    //         const response = await fetch(`/add`, {
+    //           method: 'POST',
+    //           body: JSON.stringify({ todoText }),
+    //           headers: { 'Content-Type': 'application/json' },
+    //         });
+        
+    //         if (response.ok) {
+    //           console.log("ok");
+    //           document.location.reload();
+    //         } else {
+    //           alert('Failed to add todo item');
+    //         }
+    //       }
+    }else{
+        console.log("pwds dont match")
+        document.querySelector("#pwErr").append("Passwords do not match. Try again.")
     }
-  };
+}
+    
+//   };
   
-  document.querySelector('#submit-todo-button').addEventListener('click', createTodoHandler);
-
-
-
-
-
-
-
-
-// confirm.addEventListener('click', )
+document.querySelector('#makeAccount').addEventListener('click', createAccount);
