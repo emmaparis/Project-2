81d851e57a112b786b22b9e7fd8bae3ba7a9cc0c
tatys5394 <123910162+tatys5394@users.noreply.github.com>
2023-03-26 22:36:52 -0400

    replaced  working code for calendar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,45 +1,81 @@
-const { Todos } = require("../../models");
+const calendarEl = document.getElementById("calendar");
 
-document.addEventListener("DOMContentLoaded", function () {
-  const calendarEl = document.getElementById("calendar");
-  console.log(calendarEl);
-  const calendar = new FullCalendar.Calendar(calendarEl, {
-    eventDidMount: function (info) {
-      const tooltip = document.createElement("div");
-      tooltip.textContent = "TESTING";
-      tooltip.setAttribute("role", "tooltip");
-      document.body.appendChild(tooltip);
-      console.log(info.el);
-      info.el.addEventListener("click", function (e) {
-        e.preventDefault();
-        const before = window.getComputedStyle(e.target, ":before");
-        before.innerHTML = "<h1>WOW COOL</h1>";
-        console.log(before);
-      });
-      Popper.createPopper(info.el, tooltip, {
-        placement: "left",
-      });
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    timeZone: 'UTC',
+    initialView: 'threeDays',
+    headerToolbar: {
+      left: 'prev,next',
+      center: 'title',
+      right: 'threeDays, dayGridDay'
+    },
+    views: {
+      threeDays: {
+        type: 'dayGrid',
+        duration: { days: 3 }
+      }
     },
     editable: true,
-    selectable: true,
-    select: function () {
-      alert("a day has been clicked!");
-    },
-    initialView: "dayGridWeek",
     events: [
       {
-        id: "a",
-        title: "my event",
-        start: "2023-03-24",
-      },
-    ],
-  });
-console.log(toDos)
-  calendar.addEvent({
-    id: "a",
-    title: "add event",
-    start: "2023-03-25",
+        id: 'a',
+        title: 'my event',
+        start: '2023-03-26'
+      }
+    ]
   });
 
   calendar.render();
 });
+
+
+
+
+// const { Todos } = require("../../models");
+
+// document.addEventListener("DOMContentLoaded", function () {
+  // const calendarEl = document.getElementById("calendar");
+  // console.log(calendarEl);
+  // const calendar = new FullCalendar.Calendar(calendarEl, {
+  //   eventDidMount: function (info) {
+  //     const tooltip = document.createElement("div");
+  //     tooltip.textContent = "TESTING";
+  //     tooltip.setAttribute("role", "tooltip");
+  //     document.body.appendChild(tooltip);
+  //     console.log(info.el);
+  //     info.el.addEventListener("click", function (e) {
+  //       e.preventDefault();
+  //       const before = window.getComputedStyle(e.target, ":before");
+  //       before.innerHTML = "<h1>WOW COOL</h1>";
+  //       console.log(before);
+  //     });
+  //     Popper.createPopper(info.el, tooltip, {
+  //       placement: "left",
+  //     });
+    // },
+    
+//     editable: true,
+//     selectable: true,
+//     select: function () {
+//       alert("a day has been clicked!");
+//     },
+//     initialView: "dayGridWeek",
+//     events: [
+//       {
+//         id: "a",
+//         title: "my event",
+//         start: "2023-03-24",
+//       },
+//     ],
+//   });
+// console.log(toDos)
+//   calendar.addEvent({
+//     id: "a",
+//     title: "add event",
+//     start: "2023-03-25",
+//   });
+
+//   calendar.render();
+// });
