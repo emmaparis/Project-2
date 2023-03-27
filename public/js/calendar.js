98fefd723615b73b5f39e1ebf57eef29295a2329
tatys5394 <123910162+tatys5394@users.noreply.github.com>
2023-03-26 23:34:53 -0400

    got calendar working

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,10 +1,66 @@
+// const { default: events } = require("inquirer/lib/utils/events");
+
 const calendarEl = document.getElementById("calendar");
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function() {
   var calendarEl = document.getElementById('calendar');
+  let events = [];
+  try {
+    const response = await fetch('/recurring', 
+    {
+      method: 'GET',
+    });
+  
+    if (response.ok) {
+      const todos = await response.json();
+      console.log('Fetched data:', todos); // Add this line to log the fetched data
+      let filteredList = [];
+      let todayDate = new Date();
+      for (i = 0; i < todos.length; i++) {
+        let validDays = [];
+        if (todos[i].sunday) {
+          validDays.push(0);
+        }
+        if (todos[i].monday) {
+          validDays.push(1);
+        }
+        if (todos[i].tuesday) {
+          validDays.push(2);
+        }
+        if (todos[i].wednesday) {
+          validDays.push(3);
+        }
+        if (todos[i].thursday) {
+          validDays.push(4);
+        }
+        if (todos[i].friday) {
+          validDays.push(5);
+        }
+        if (todos[i].saturday) {
+          validDays.push(6);
+        }
+        let todayDay = todayDate.getDay();
+        if (validDays.includes(todayDay)) {
+          filteredList.push(todos[i])
+        }
+      } console.log(filteredList);
+      for (i = 0; i < filteredList.length; i++) {
+        const event = {
+          id: filteredList[i].todo_item,
+          title: filteredList[i].todo_item,
+          start: `${todayDate.getFullYear()}-${(todayDate.getMonth() +1).toString().padStart(2,"0")}-${todayDate.getDate()}`
+        }
+        events.push(event);
+      } console.log(events);
+    } else {
+      alert('Failed to fetch recurring todos.');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    timeZone: 'UTC',
+    timeZone: 'EST',
     initialView: 'threeDays',
     headerToolbar: {
       left: 'prev,next',
@@ -18,13 +74,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     },
     editable: true,
-    events: [
-      {
-        id: 'a',
-        title: 'my event',
-        start: '2023-03-26'
-      }
-    ]
+    events: events
   });
 
   calendar.render();
